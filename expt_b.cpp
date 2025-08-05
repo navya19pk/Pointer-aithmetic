1230dfedb552c1a7201a260100a652083f76077e
navya <navyapkhajuria@gmail.com>
2025-08-05 15:22:01 +0530

    Create expt_b.cpp

---
+++ expt_b.cpp
@@ -0,0 +1,25 @@
+#Navya Pawan Khajuria
+#24070123151
+#B3
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   int arr[]={10,20,30};
+   cout<<*arr<<endl;
+   int *ptr=arr;
+   for(int i=0; i<3; i++)
+   {
+       cout<<*ptr<<endl;
+       ptr++;
+   }
+    return 0;
+}
+
+#Output:
+#10
+#10
+#20
+#30
+
