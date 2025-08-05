c884e77c7a8a8f0aa21919b4c3e550ff6110c1b5
navya <navyapkhajuria@gmail.com>
2025-08-05 15:28:47 +0530

    Create expt_d.cpp

---
+++ expt_d.cpp
@@ -0,0 +1,15 @@
+#Navya Pawan Khajuria
+#24070123151
+#B3
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[]={10,20,30};
+    cout<<*arr<<endl;
+    return 0;
+}
+
+#Output:
+#10
