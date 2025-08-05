29c463fa7835dfb8ba1bc7a2f772732f5cf8f9a4
navya <navyapkhajuria@gmail.com>
2025-08-05 15:14:08 +0530

    Create exp_a.cpp

---
+++ exp_a.cpp
@@ -0,0 +1,24 @@
+#Navya Pawan Khajuria
+#24070123151
+#B3
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a =10;
+    int*aptr;
+    aptr=&a;
+    cout<< a <<endl;
+    cout<<*aptr<<endl;
+    cout<<aptr<<endl;
+    cout<<&a<<endl;
+    return 0;
+}
+
+
+#Output:
+#10
+#10
+#0x7fff3977f874
+#0x7fff3977f874
